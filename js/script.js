83c4b67e8c111ee54dbab9e8eeb7b778f2182b9c
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-01-30 10:30:17 +0100

    correzione esercizio

--- js/script.js
+++ js/script.js
@@ -8,30 +8,56 @@
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 
-let kilomitres = prompt("Chilometri vuoi percorrere?");
-
-console.log("kilomitres");
-
-let age = prompt("quanti anni hai?");
-
-console.log("age")
-
-let price = 0.21 * kilomitres ;
-
-/* price = Math.round((price * 100 ) / 100 ).toFixed(2); */
-
-let priceUnder = (price - ((price * 20) / 100));
-
-let priceOver = (price - ((price * 40) / 100));
+// chiedo asll'utende il numero di chilometri e la memorizzo in una variabile 
+// chiedo all'utente l'età e la memorizzo
+// genero un prezzo base calcolato moltiplicando km * 0.21
+// SE l'età è < 18// - allora applica sconro del 20%
+// altrimetni se l'età >= 65 - allora applicasconto del 40%
+// approssimare il prezzo a due cifre decimali
+// stampare in pagina
 
 
 
+/* let kilomitres = prompt("Chilometri vuoi percorrere?");
+kilomitres = Number(kilomitres); */
+const kilometres = Number(prompt("quanti chilometri vuoi fare?"));
+/* console.log("kilomitres"); */
+if(isNaN(kilometres) || kilometres < 1){
+    alert("Hai inserito un numero errato,ricarica la pagina e inserisci un valore corretto");
+}
+/* let age = prompt("quanti anni hai?");
+age = Number(age); */
+
+// si può scrivere direttamente su una riga sola:
+
+const age = Number ( prompt("Quanti anni hai?") );
+/* console.log("age") */
+if(isNaN(age) || age < 0 || age > 122){
+    alert("Hai inserito un numero errato,ricarica la pagina e inserisci un valore corretto");
+}
+let price = 0.21 * kilometres;
+
+
+/* let priceUnder = Math.round(((price - ((price * 20) / 100)) * 100) /100).toFixed(2);
+
+let priceOver = Math.round(((price - ((price * 40) / 100)) * 100) /100).toFixed(2); */
+
+
+let finalPrice = price;
+let discountMessage = ""; //ho inserito una stringa vuota
 /* document.getElementById("prezzo").innerHTML = `Il prezzo del biglietto è "${price}" per gli under è "${priceUnder}" per gli over è "${priceOver}"`; */
 
 if (age < 18){
-    document.getElementById("prezzo").innerHTML = `Il prezzo è "${priceUnder}" `
-} else if (age < 65){
-    document.getElementById("prezzo").innerHTML = `Il prezzo è "${price}" `
-} else {
-    document.getElementById("prezzo").innerHTML = `Il prezzo è "${priceOver}" `
+    //allora fai sconto del 20%
+    finalPrice = price - price * 0.2;
+    discountMessage = "E' stato apllicato uno sconto del 20% perchè sei minorenne."
+
+/*     document.getElementById("prezzo").innerHTML = `Il prezzo è "${finalPrice}" ` */
+
+} else if (age >= 65){
+    //allora sconto 40%
+    finalPrice = price - price * 0.4
+/*     document.getElementById("prezzo").innerHTML = `Il prezzo è "${finalPrice}" `*/
 }
+
+document.getElementById("prezzo").innerHTML = `Il prezzo è ${finalPrice.toFixed(2)} <br> ${discountMessage}`
