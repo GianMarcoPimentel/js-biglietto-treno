9c1ee86749a01899e8befe459d5059282a248f2f
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-01-30 15:36:32 +0100

    Fine esercizio CartaForbiceSasso

--- :snack/Carte-forbice-sasso/js/script.js
+++ :snack/Carte-forbice-sasso/js/script.js
@@ -1,12 +1,44 @@
-const X = prompt("Scegli 1 , 2 o 3");
-console.log("SceltaUtente" ,X);
-const Y = Math.floor( Math.random() * 3);
-console.log("SceltaComputer", Y)
+const sceltaUtente = prompt("Scegli Carta Forbice Sasso");
+console.log("sceltaUtente" ,sceltaUtente)
 
-if (X=Y){
-    document.getElementById("risultato").innerHTML = `La scelta è la stessa. Pareggioo. Gioca di nuovo`
-} else if (X == 1 && Y== 2) or (X == 2 && Y==3) {
-    document.getElementById("risultato").innerHTML = `Hai perso`
-} else if {
-    
+//facciamo scegliere ranndomicamente al computer
+//possimo avere una variabile che sia uguale a un numro intero per la scelta random dove:
+// il valore 0 corrisponderà a carta
+// il valore 1 corrisponderà a  forbici
+// il valore 2 corrisponderà a sasso
+const sceltaComputer = Math.floor( Math.random() * 3);
+
+// introduco la variabile conversioneComputer 
+// per assegnare al numero il valore di una stringa
+let conversioneComputer;
+ // conversione da numero a strigna
+if(sceltaComputer == 1) {
+    conversioneComputer = "carta";
+} else if (sceltaComputer == 2){
+    conversioneComputer = "forbici";
+} else {
+    conversioneComputer = "sasso";
+}
+console.log("sceltaComputer" , sceltaComputer)
+
+if(sceltaUtente == sceltaComputer){
+    document.getElementById("risultato").innerHTML = `Avete pareggiato!`
+} else if (sceltaUtente == "carta") {
+    if (conversioneComputer == "sasso"){
+    document.getElementById("risultato").innerHTML = `Hai vinto!`
+    } else {
+        document.getElementById("risultato").innerHTML = `Hai Perso!`
+    }
+} else if (sceltaUtente == "forbici"){
+    if(conversioneComputer == "carta"){
+    document.getElementById("risultato").innerHTML = `Hai vinto!`
+    }else {
+        document.getElementById("risultato").innerHTML = `Hai Perso!`
+    }
+} else{
+    if(conversioneComputer == "forbici"){
+        document.getElementById("risultato").innerHTML = `Hai vinto!`
+        }else {
+            document.getElementById("risultato").innerHTML = `Hai Perso!`
+        }
 }
